08da4b3142caaeaf78bf4afffe88cfa09f777309
Leandro Augusto Lelis <76403096+leandroaugustolelis@users.noreply.github.com>
2021-01-13 16:21:18 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 /**
- * Babel: Converter (transpilar) código do React mais atualizado para um código que o browser entenda
- * WebPack:Para cada tipo de arquivo(.js, .css, .png ) eu vou converter o código de uma maneira diferente
+ * BABEL: Converter (transpilar) código do React mais atualizado para um código que o browser entenda
+ * WEBPACK:Para cada tipo de arquivo(.js, .css, .png ) eu vou converter o código de uma maneira diferente
  * 
- * Loaders: babel-loader, css-loader, image-loader
+ * LOADERS: babel-loader, css-loader, image-loader
  */
